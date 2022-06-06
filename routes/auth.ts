bad11e074a84672b3185a04ba3ee820149b71491
Pranjal Timsina <pranjal.timsina@live.com>
2022-06-06 20:22:34 +0545

    fix: res head being written after res sent to client when logging in as a new user

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response, Router } from 'express'
+import express, { NextFunction, Request, Response, Router } from 'express'
+import { LoginTicket, TokenPayload } from 'google-auth-library'
 import jwt from 'jsonwebtoken'
 
-import verify from '../middlewares/ticket'
+import verifyGoogleUser from '../middlewares/ticket'
 
 import User from '../models/user'
 
@@ -9,9 +10,9 @@ const JWT_SECRET: string = process.env.JWT_SECRET || 'shhh'
 
 const router: Router = express.Router()
 
-router.post('/google', verify, async (req: Request, res: Response) => {
+router.post('/google', verifyGoogleUser, async (req: Request, res: Response) => {
   try {
-    const ticket = res.locals.ticket
+    const ticket: LoginTicket = res.locals.ticket
 
     const ticketPayload = ticket.getPayload()
 
@@ -33,16 +34,19 @@ router.post('/google', verify, async (req: Request, res: Response) => {
         picture: ticketPayload?.picture
       })
 
-      newUser.save((saveErr: any, savedUser: any) => {
-        return res.status(201).json({
-          data: savedUser,
-          token: newToken
-        })
+      await newUser.save((saveErr: any, savedUser: any) => {
+        if (saveErr) {
+          res.status(502).json({error: "Could not create user"})
+        } else {
+          return res.status(201).json({
+            data: savedUser,
+            token: newToken
+          })
+        }
       })
+    } else {
+      return res.status(200).json({data: currentUser, token: newToken})
     }
-
-
-    return res.status(200).json({data: currentUser, token: newToken})
   } catch (err) {
     console.log(err)
     return res.status(401).json({error: "Unauthorized."})
