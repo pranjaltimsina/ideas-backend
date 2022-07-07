7983dd9875be80659329e42803bbeaa44b6ac4c5
Pranjal Timsina <pranjal.timsina@live.com>
2022-07-07 11:42:40 +0545

    feat: use verifyUrlParams middleware in users route

--- routes/user.ts
+++ routes/user.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express'
 
 import jwt from '../middlewares/jwt'
+import verifyParams from '../middlewares/verifyParams'
 
 import * as userController from '../controllers/userController'
 
@@ -8,10 +9,10 @@ const router: Router = express.Router()
 
 router.use(jwt)
 
-router.get('/:userId/comments', userController.getUserComments)
+router.get('/:userId/comments', verifyParams(['userId']), userController.getUserComments)
 
-router.get('/:userId/ideas', userController.getUserIdeas)
+router.get('/:userId/ideas', verifyParams(['userId']), userController.getUserIdeas)
 
-router.get('/:userId', userController.getUserProfile)
+router.get('/:userId', verifyParams(['userId']), userController.getUserProfile)
 
 export default router
