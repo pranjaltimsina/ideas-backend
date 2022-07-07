48d0b1ac72ef4a6ba33404f12f99efbe1a0cd835
Pranjal Timsina <pranjal.timsina@live.com>
2022-07-07 18:45:13 +0545

    feat: impl getUserComments and getUserIdeas

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,20 +1,33 @@
 import { Request, Response } from 'express'
 import Comment from '../models/comment'
+import Idea from '../models/idea'
 
 const getUserComments = async (req: Request, res: Response) => {
   const userId: string = res.locals.userId || ''
 
-  const comments = await Comment.find({
-    author: userId
-  }).lean()
+  try {
+    const comments = await Comment.find({
+      author: userId
+    }).lean()
 
-  console.log(comments)
-
-  return res.status(501).end()
+    return res.status(200).json({ comments })
+  } catch {
+    return res.status(500).json({ error: 'Could not fetch comments by user.' })
+  }
 }
 
-const getUserIdeas = (req: Request, res: Response) => {
-  return res.status(501).end()
+const getUserIdeas = async (req: Request, res: Response) => {
+  const userId: string = res.locals.userId || ''
+
+  try {
+    const ideas = await Idea.find({
+      author: userId
+    }).lean()
+
+    return res.status(200).json({ ideas })
+  } catch {
+    return res.status(500).json({ error: 'Could not fetch ideas by user.' })
+  }
 }
 
 const getUserProfile = (req: Request, res: Response) => {
