4687100ef59aab5615865f0ac1795960428df8ff
Pranjal Timsina <pranjal.timsina@live.com>
2022-07-08 08:33:28 +0545

    feat: impl get unapproved ideas handler

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express'
+import mongoose from 'mongoose'
 import Comment from '../models/comment'
 import Idea from '../models/idea'
 import User from '../models/user'
 
-const getUserComments = async (req: Request, res: Response) => {
+const getUserComments = async (req: Request, res: Response): Promise<Response> => {
   const userId: string = res.locals.userId || ''
 
   try {
@@ -17,7 +18,7 @@ const getUserComments = async (req: Request, res: Response) => {
   }
 }
 
-const getUserIdeas = async (req: Request, res: Response) => {
+const getUserIdeas = async (req: Request, res: Response): Promise<Response> => {
   const userId: string = res.locals.userId || ''
 
   try {
@@ -31,7 +32,29 @@ const getUserIdeas = async (req: Request, res: Response) => {
   }
 }
 
-const getUserProfile = async (req: Request, res: Response) => {
+const getUnapprovedIdeas = async (req: Request, res: Response): Promise<Response> => {
+  const userId: string = res.locals.userId || ''
+  const authorizationId: string = res.locals.user.id || ''
+
+  if (!new mongoose.Types.ObjectId(userId).equals(authorizationId)) {
+    return res.status(404).json({ error: 'Bad Request. You can only get your own unapproved ideas.' })
+  }
+
+  try {
+    const unapprovedIdeas = await Idea.find({
+      author: userId,
+      approved: false
+    }).lean()
+
+    return res.status(200).json({
+      unapprovedIdeas
+    })
+  } catch {
+    return res.status(500).json({ error: 'Could not fetch unapproved ideas.' })
+  }
+}
+
+const getUserProfile = async (req: Request, res: Response): Promise<Response> => {
   const userId: string = res.locals.userId || ''
 
   try {
@@ -48,5 +71,6 @@ const getUserProfile = async (req: Request, res: Response) => {
 export {
   getUserComments,
   getUserIdeas,
+  getUnapprovedIdeas,
   getUserProfile
 }
