bace4c1ce585fa999445389aeb537b264a2655ca
Pranjal Timsina <pranjal.timsina@live.com>
2022-08-18 12:01:31 +0530

    feat: make get all ideas an open endpoint

--- routes/ideas.ts
+++ routes/ideas.ts
@@ -8,28 +8,28 @@ import verifyPathParams from '../middlewares/verifyPathParams'
 
 const router: Router = express.Router()
 
-router.use(jwt)
+// router.use(jwt)
 
 router.get('/', ideaController.getAllIdeas)
 
-router.post('/', ideaController.createIdea)
+router.post('/', jwt, ideaController.createIdea)
 
-router.get('/:ideaId', verifyPathParams(['ideaId']), ideaController.getIdeaById)
+router.get('/:ideaId', jwt, verifyPathParams(['ideaId']), ideaController.getIdeaById)
 
-router.put('/:ideaId', verifyPathParams(['ideaId']), ideaController.editIdea)
+router.put('/:ideaId', jwt, verifyPathParams(['ideaId']), ideaController.editIdea)
 
-router.delete('/:ideaId', verifyPathParams(['ideaId']), ideaController.deleteIdea)
+router.delete('/:ideaId', jwt, verifyPathParams(['ideaId']), ideaController.deleteIdea)
 
-router.get('/user/:userId', verifyPathParams(['userId']), ideaController.getIdeaByUserId)
+router.get('/user/:userId', jwt, verifyPathParams(['userId']), ideaController.getIdeaByUserId)
 
-router.patch('/:ideaId/vote', verifyPathParams(['ideaId']), ideaController.voteIdea)
+router.patch('/:ideaId/vote', jwt, verifyPathParams(['ideaId']), ideaController.voteIdea)
 
-router.post('/:ideaId/comments', verifyPathParams(['ideaId']), commentController.addComment)
+router.post('/:ideaId/comments', jwt, verifyPathParams(['ideaId']), commentController.addComment)
 
-router.get('/comments/:commentId', verifyPathParams(['commentId']), commentController.getReplies)
+router.get('/comments/:commentId', jwt, verifyPathParams(['commentId']), commentController.getReplies)
 
-router.delete('/comments/:commentId', verifyPathParams(['commentId']), commentController.deleteComment)
+router.delete('/comments/:commentId', jwt, verifyPathParams(['commentId']), commentController.deleteComment)
 
-router.put('/comments/:commentId', verifyPathParams(['commentId']), commentController.editComment)
+router.put('/comments/:commentId', jwt, verifyPathParams(['commentId']), commentController.editComment)
 
 export default router
